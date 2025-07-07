9eba07924388006b280d238ac5dd9713bb5e54b4
mandz1952 <mandz050505@gmail.com>
2025-07-07 23:53:41 +0300

    fix plus button

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -13,7 +13,7 @@ export function Header (){
 
     return (
         <div className={'mb-3'}>
-            <div className="flex items-center justify-between px-4 py-4 gap-1">
+            <div className="flex items-center justify-between px-3 py-3 gap-1">
                 <div className="w-16 h-16 rounded-lg flex items-center justify-center ">
                     <div className="text-xs text-center">
                         <Image src={'/logo.svg'} width={62} height={48} alt="logo"/>
@@ -25,7 +25,7 @@ export function Header (){
                         className="bg-[#231c46] border-2 border-[#533189] rounded-lg px-2 py-1 flex items-center gap-2">
                         <Image src={'/tonCoin.svg'} alt={"ton Coin"} width={18} height={18}/>
                         <span className="text-white text-lg font-semibold">0.00</span>
-                        <Button className="bg-[#21ee43] hover:bg-[#21ee43]/90 text-black w-5 h-5 p-0 mr-2">
+                        <Button className="bg-[#21ee43] hover:bg-[#21ee43]/90 text-black w-5 h-5 p-0 mr-4">
                             <Plus className="w-4 h-4"/>
                         </Button>
                     </div>
