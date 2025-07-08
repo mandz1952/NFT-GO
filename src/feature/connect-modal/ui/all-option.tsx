4d4b5da28ab964e8fcc5572d681e76be875c2972
mandz1952 <mandz050505@gmail.com>
2025-07-08 16:15:09 +0300

    fix bugs

--- src/feature/connect-modal/ui/all-option.tsx
+++ src/feature/connect-modal/ui/all-option.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function AllOption({wallet}: {wallet: string}) {
     return (
         <div
-            className="flex flex-col items-center gap-2  rounded-lg cursor-pointer transition-colors"
+            className="flex flex-col items-center gap-1 rounded-lg cursor-pointer transition-colors"
         >
             <div className="w-20 h-20  rounded-lg flex items-center justify-center">
                 <Image src={'/tonkeeper.svg'} alt={'tonkeeper'} width={65} height={65} />
