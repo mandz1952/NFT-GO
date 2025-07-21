f2d3595e642fcdb84b46ad6684ba0fa1650731e9
mandz1952 <mandz050505@gmail.com>
2025-07-21 15:56:52 +0300

    fix bugs

--- src/shared/lib/ParamsRoute.tsx
+++ src/shared/lib/ParamsRoute.tsx
@@ -3,14 +3,6 @@
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-const Menu = {
-    MARKET: '',
-    TOP: 'top',
-    ROCKET: 'rocket',
-    FRIENDS: 'friends',
-    PROFILE: 'profile',
-};
-
 export default function ParamsRoute() {
     const router = useRouter();
     const searchParams = useSearchParams();
@@ -33,32 +25,9 @@ export default function ParamsRoute() {
         }
 
         if (startParam !== null) {
-            switch (startParam) {
-                case Menu.MARKET:
-                    console.log('Redirecting to /');
-                    router.replace('/', { scroll: false });
-                    break;
-                case Menu.TOP:
-                    console.log('Redirecting to /top');
-                    router.replace('/top', { scroll: false });
-                    break;
-                case Menu.ROCKET:
-                    console.log('Redirecting to /rocket');
-                    router.replace('/rocket', { scroll: false });
-                    break;
-                case Menu.FRIENDS:
-                    console.log('Redirecting to /friends');
-                    router.replace('/friends', { scroll: false });
-                    break;
-                case Menu.PROFILE:
-                    console.log('Redirecting to /profile');
-                    router.replace('/profile', { scroll: false });
-                    break;
-                default:
-                    console.log(`Redirecting to default / for param: ${startParam}`);
-                    router.replace('/', { scroll: false });
-                    break;
-            }
+            const path = startParam.startsWith('/') ? startParam : `/${startParam}`;
+            console.log(`Redirecting to ${path}`);
+            router.replace(path, { scroll: false });
         } else {
             console.log('No startapp parameter, staying on current page');
         }
