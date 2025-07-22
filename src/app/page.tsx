0dd2c86a1b00adede338b105e60cf2ba1721276f
mandz1952 <mandz050505@gmail.com>
2025-07-22 13:14:56 +0300

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import ParamsRoute from "@/shared/lib/ParamsRoute";
 import {Suspense} from "react";
 
 export default function Home() {
-  return (
-    <div className={'bg-[#150f27] min-h-screen text-white mx-auto relative'}>
-        <Suspense fallback={null}>
-            <ParamsRoute />
-        </Suspense>
-        <Filter/>
-        <NftList isProfile={false} imgNft={'/nft-1.png'}/>
-    </div>
-  );
+    return (
+        <div className={'bg-[#150f27] min-h-screen text-white mx-auto relative'}>
+            <ParamsRoute/>
+            <Filter/>
+            <NftList isProfile={false} imgNft={'/nft-1.png'}/>
+        </div>
+    );
 }
